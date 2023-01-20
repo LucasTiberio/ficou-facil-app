f5821a4a631d19b6a7ce743ad21626c8ff133633
Lucas Tiberio <ext.lucas.tiberio@farfetch.com>
2023-01-20 06:54:36 -0300

    fix: Fix fetch history cached condition

--- src/hooks/integrations/useFetchHistoryIntegration.ts
+++ src/hooks/integrations/useFetchHistoryIntegration.ts
@@ -11,7 +11,7 @@ const useFetchHistoryIntegration = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            if (!!historic) {
+            if (!!historic.length) {
                 return;
             }
 
